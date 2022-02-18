4eb0a8cc7185de1ca49f7a7c0420d95e83bf30de
Sergio19702 <sergiusutu02@gmail.com>
2022-02-18 14:15:06 +0100

    Commit

---
+++ S4/Progetto/assets/js/script.js
@@ -0,0 +1,11 @@
+function numero(dato){ 
+    document.getElementById("calcolatrice").value+=dato;
+ }
+ 
+ function operazione() { 
+ document.getElementById("calcolatrice").value=eval(document.getElementById("calcolatrice").value); 
+ }
+ 
+ function cancella() { 
+  document.getElementById("calcolatrice").value=""; 
+ }
